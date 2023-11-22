3683066b41050fe5201393c2ed605bea9fdace91
lejdak <lejda.kolaveri@studenti.unimi.it>
2023-11-22 14:16:31 +0100

    update

--- lejda.kolaveri/hotel.project/hotel/project/main.py
+++ lejda.kolaveri/hotel.project/hotel/project/main.py
@@ -5,6 +5,15 @@ from preferences_allocation import PreferencesAllocator
 from price_allocation import PriceBasedAllocator
 from availability_allocation import AvailabilityBasedAllocator
 
+def output(allocation):
+    print('customers accomodated: ', allocation['guest_id'].count())
+    print('number of rooms occupied: ', allocation['guest_id'].count())
+    unique_hotels = allocation['hotel_id'].unique()
+    num_unique_hotels = len(unique_hotels)
+    print('Number of different hotels occupied:', num_unique_hotels)
+    print('Average satisfaction:', round(allocation['satisfaction_percentage'].mean(),2))
+    total_earnings = allocation[['hotel_id', 'paid_price']].copy()
+    print('Total earnings per hotel:', total_earnings.groupby('hotel_id').sum(), sep='\n')
 
 def main():
     guests= pd.read_excel(r"C:\Users\lejda\Desktop\coding - Python\guests.xlsx")
@@ -14,20 +23,24 @@ def main():
     print('calculate random allocation')
     random_allocator=RandomHotelAllocation(hotels, guests, preferences)
     r_allocation = random_allocator.get_r_allocation()
+    output(r_allocation)
     visualization(r_allocation.head(20))
     
     print('calculate preferences allocation')
     preferences_allocator=PreferencesAllocator(hotels, guests, preferences)
     cp_allocation=preferences_allocator.get_cp_allocation()
+    output(cp_allocation)
     visualization(cp_allocation.head(20))
     
     print('calculate price allocation')
     price_allocator=PriceBasedAllocator(hotels, guests, preferences)
     p_allocation=price_allocator.get_p_allocation()
+    output(p_allocation)
     visualization(p_allocation.head(20))
     
     print('calculate availability allocation')
     availability_allocator=AvailabilityBasedAllocator(hotels, guests, preferences)
     a_allocation=availability_allocator.get_a_allocation()
+    output(a_allocation)
     visualization(a_allocation.head(20))
     
--- lejda.kolaveri/hotel.project/hotel/project/utils.py
+++ lejda.kolaveri/hotel.project/hotel/project/utils.py
@@ -13,7 +13,7 @@ class satisfaction:
         if guest_preferences.empty:
             return 100  # No preferences, 100% satisfaction
 
-        index_of_preference = (guest_preferences['hotel'] == hotel_id).idxmax() # Find the index of the allocated hotel in the guest's preferences
+        index_of_preference = (guest_preferences['hotel'] == hotel_id).idxmax() # Find the index of the allocated hotel in the guest's         preferences
         satisfaction = round(((len(guest_preferences) - index_of_preference) / len(guest_preferences)) * 100)
         return satisfaction if satisfaction >= 0 else 0
 
