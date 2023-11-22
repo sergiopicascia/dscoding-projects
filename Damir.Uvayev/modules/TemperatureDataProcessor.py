7fbf3d8bfa62a213fa546305ff429de3138965f4
Demi-ux670 <82157132+Demi-ux670@users.noreply.github.com>
2023-11-22 17:34:43 +0100

    Update TemperatureDataProcessor.py

--- Damir.Uvayev/modules/TemperatureDataProcessor.py
+++ Damir.Uvayev/modules/TemperatureDataProcessor.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class TemperatureDataProcessor:
     def __init__(self, file_path):
         self.data = self.load_data(file_path)
@@ -8,13 +9,13 @@ class TemperatureDataProcessor:
     def load_data(file_path):
         return pd.read_csv(file_path)
 
-    def filter_data_by_period(self, start_date, end_date):
-        mask = (self.data['dt'] >= start_date) & (self.data['dt'] <= end_date)
-        return self.data[mask]
-
     @staticmethod
     def compute_city_avg_temperature(data):
         return data.groupby('City')['AverageTemperature'].mean().reset_index()
+
+    def filter_data_by_period(self, start_date, end_date):
+        mask = (self.data['dt'] >= start_date) & (self.data['dt'] <= end_date)
+        return self.data[mask]
 
     def merge_avg_with_original(self, avg_temperatures):
         merged_data = avg_temperatures.merge(self.data[['City', 'Latitude', 'Longitude']], on='City', how='left')
