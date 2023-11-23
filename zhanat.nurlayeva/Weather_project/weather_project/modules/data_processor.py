cc5bdbbf87fd4d662dd7d3cc30fb4f3a98ef315f
nurlayevazhanat <zhanatnurlayeva@gmail.com>
2023-11-23 18:08:07 +0100

    Update data_processor.py

--- zhanat.nurlayeva/Weather_project/weather_project/modules/data_processor.py
+++ zhanat.nurlayeva/Weather_project/weather_project/modules/data_processor.py
@@ -10,6 +10,10 @@ class DataProcessor:
     def load_data(self, file_path):
         """Load data from a CSV file."""
         self.data = pd.read_csv(file_path)
+        # Convert 'dt' column to datetime if present
+        if 'dt' in self.data.columns and not pd.api.types.is_datetime64_any_dtype(self.data['dt']):
+            self.data['dt'] = pd.to_datetime(self.data['dt'])
+
 
     def compute_city_avg_temperature(self):
         """Compute the average temperature for each city."""
@@ -21,10 +25,6 @@ class DataProcessor:
 
         return avg_temperatures
 
-    #def compute_city_avg_temperature(self):
-        #"""Compute the average temperature for each city."""
-        #return self.data.groupby('City')['AverageTemperature'].mean().reset_index()
-
     def clean_data(self):
         """Clean and preprocess data."""
         self.data.dropna(subset=['AverageTemperature'], inplace=True)
@@ -35,5 +35,14 @@ class DataProcessor:
         return merged_data.drop_duplicates(subset='City')
 
     def filter_data_by_period(self, start_date, end_date):
+        # Ensure 'dt' column is converted to datetime
+        if 'dt' not in self.data.columns or not pd.api.types.is_datetime64_any_dtype(self.data['dt']):
+            self.load_data(file_path)
+
+        # Convert start_date and end_date to datetime objects
+        start_date = pd.to_datetime(start_date)
+        end_date = pd.to_datetime(end_date)
+
         mask = (self.data['dt'] >= start_date) & (self.data['dt'] <= end_date)
         return self.data[mask]
+
